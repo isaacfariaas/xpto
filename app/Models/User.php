bec4c7e74b03315be4a62ab5b0778fab8c7beac6
isaacfariaas <farias.isaac321@gmail.com>
2023-03-15 22:05:15 -0300

    feat: add function subscribes to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -54,4 +54,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function subscribes()
+    {
+        return $this->hasMany(Subscribe::class, 'id_user');
+    }
 }
